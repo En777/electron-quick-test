eb842941469d539591145b8b43623d2d5420e05c
En <En>
2021-12-25 23:47:21 +0800

    init

---
+++ src/renderer/index.js
@@ -0,0 +1,11 @@
+// import { shell } from 'electron'
+
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true'
+
+document.body.innerHTML += `
+<pre>
+quick test at electron app environment.
+</pre>
+<br>
+<a href="https://github.com/En777/electron-quick-test">github source</a>
+`
